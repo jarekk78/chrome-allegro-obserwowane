2c73bfd71d2ee9344f9eef814f4bc52267d65dc5
Jaroslaw Kacerka <kacerkaj@gmail.com>
2013-09-24 16:12:54 +0200

    clean up, added packed extension (crx)

---
+++ source/background.js
@@ -0,0 +1,37 @@
+function genericOnClick0(info, tab) {
+	chrome.tabs.executeScript(tab.id,{"allFrames":true, "file":"checkall.js"});
+}
+function genericOnClick1(info, tab) {
+	chrome.tabs.executeScript(tab.id,{"allFrames":true, "file":"checkall1.js"});
+}
+function genericOnClick2(info, tab) {
+	chrome.tabs.executeScript(tab.id,{"allFrames":true, "file":"checkall2.js"});
+}
+
+var pat = "http://allegro.pl/myaccount/watch.php";
+var id = chrome.contextMenus.create({
+	"documentUrlPatterns":[pat], 
+	"title": "Allegro - obserwowane", 
+	"contexts":[ "page" ] 
+	});
+chrome.contextMenus.create({
+	"parentId":id, 
+	"documentUrlPatterns":[pat], 
+	"title": "e-mail: zaznacz wszystkie", 
+	"contexts":[ "page" ], 
+	"onclick": genericOnClick0
+	});
+chrome.contextMenus.create({
+	"parentId":id, 
+	"documentUrlPatterns":[pat], 
+	"title": "e-mail: zaznacz bez powiadomień", 
+	"contexts":[ "page" ], 
+	"onclick": genericOnClick1
+	});
+chrome.contextMenus.create({
+	"parentId":id, 
+	"documentUrlPatterns":[pat], 
+	"title": "odznacz wszystkie", 
+	"contexts":[ "page" ], 
+	"onclick": genericOnClick2
+	});
---
+++ source/checkall.js
@@ -0,0 +1,5 @@
+var elements = document.getElementsByClassName("mailInvert"), n = elements.length;
+for (var i = 0; i < n; i++) { 
+     var e = elements[i];
+     e.checked = true;
+}
