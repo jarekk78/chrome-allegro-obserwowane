d334e9503f91f20162167b5cf6ef119f253d80e1
Jaroslaw Kacerka <kacerkaj@gmail.com>
2013-09-24 16:14:44 +0200

    clean up, added packed extension (crx)

--- checkall1.js
+++
@@ -1,5 +0,0 @@
-var elements = document.getElementsByClassName("mailInvert"), n = elements.length;
-for (var i = 0; i < n; i++) { 
-     var e = elements[i];
-     if (!(e.parentNode.innerHTML.indexOf( " godz.")>-1)) e.checked = true;
-}
--- checkall2.js
+++
@@ -1,5 +0,0 @@
-var elements = document.getElementsByClassName("mailInvert"), n = elements.length;
-for (var i = 0; i < n; i++) { 
-     var e = elements[i];
-     e.checked = false;
-}
